8b7b125994ed4e22c883d1d5ded4f4b9ad04bc71
hungweiwu <wuxx1045@umn.edu>
2017-12-12 15:03:17 -0600

    plotting w axis and edits

--- plot.py
+++ plot.py
@@ -2,15 +2,17 @@ import matplotlib.pyplot as plt
 import collections
 
 def main():
-    f = open('scores.txt', 'r')
+    f = open('moves.txt', 'r')
     lines = f.readlines()
     x = [i for i in range(len(lines))]
     y = []
     for line in lines:
-        # counter = collections.Counter(line)
-        # y.append(counter.most_common(1)[0][1])
-        y.append(int(line))
+        counter = collections.Counter(line)
+        y.append(counter.most_common(1)[0][1])
+        # y.append(int(line))
     plt.scatter(x, y)
+    plt.xlabel('Episode number', fontsize=12)
+    plt.ylabel('Most common action count', fontsize=12)
     plt.show()
 
 if __name__ == '__main__':
